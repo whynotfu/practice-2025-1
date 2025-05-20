b89fc68ac2f09c7a9468c82066fe89c43460110a
whynotfu <anadtasyadu.0.o@gmail.com>
2025-05-20 11:36:57 +0300

    emulator can read set of the commands

--- src/main.c
+++ src/main.c
@@ -3,17 +3,29 @@
 #include <stdlib.h>
 #include "cpu.h"
 
-
 int SDL_main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s rom.gb\n", argv[0]);
+        return 1;
+    }
+    
+    FILE *rom = fopen(argv[1], "rb");
+    if (!rom) {
+        perror("Failed to open ROM");
+        return 1;
+    }
+
     CPU cpu;
     cpu_init(&cpu);
 
+    fread(cpu.memory + 0x100, 1, 0x8000, rom);
+    fclose(rom);
+
     printf("Starting CPU execution...\n");
 
-    // Симуляция бесконечного выполнения команд
     while (1) {
         cpu_step(&cpu);
-        SDL_Delay(100); // Чтобы не грузить процессор полностью
+        SDL_Delay(100); // чтобы замедлить выполнение
     }
 
     return 0;
